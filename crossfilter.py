4e12b3b5b6b42727dda59f5eca022bd85c906d44
ruoyu0088 <ruoyu0088@gmail.com>
2014-11-24 10:45:07 +0900

    add ChartCommandParser

--- crossfilter.py
+++ crossfilter.py
@@ -23,14 +23,39 @@ NOTEBOOK_URLS = {
     "css": "/nbextensions/dc.css"
 }
 
+REDUCE_MEAN = """reduce(function(p, v) {{
+    p.sum = p.sum + v["{name}"];
+    p.count = p.count + 1;
+    return p;
+}},
+
+function(p, v) {{
+    p.sum = p.sum - v["{name}"];
+    p.count = p.count - 1;
+    return p;
+}},
+
+function() {{
+    return {{
+        sum: 0,
+        count: 0
+    }};
+}})"""
+
 
 class DCChart(object):
 
-    def __init__(self, df, dim_column, group_column=None, **kw):
-        self.df = df
+    def __init__(self, dim_column, group_column=None, **kw):
         self._dim_column = dim_column
         self._group_column = group_column if group_column is not None else dim_column
         self.settings = kw
+
+    def get_setting(self, key, default):
+        return self.settings.get(key, self.filter.settings.get(key, default))
+
+    @property
+    def df(self):
+        return self.filter.df
 
     @property
     def dtype(self):
@@ -58,16 +83,21 @@ class DCChart(object):
         return "c{}".format(index)
 
     @property
+    def json_group_column(self):
+        index = self.df.columns.tolist().index(self.group_column)
+        return "c{}".format(index)
+
+    @property
     def width(self):
-        return self.settings.get("width", 300)
+        return self.get_setting("width", 300)
 
     @property
     def height(self):
-        return self.settings.get("height", 300)
+        return self.get_setting("height", 300)
 
     @property
     def title(self):
-        return self.settings.get("title", self.dim_column)
+        return self.get_setting("title", self.dim_column)
 
     @property
     def min(self):
@@ -76,6 +106,22 @@ class DCChart(object):
     @property
     def max(self):
         return self.df[self.dim_column].max()
+
+    @property
+    def reduce(self):
+        method = self.reduce_method
+        if method == "count":
+            return 'reduceCount()'
+        elif method == "sum":
+            return 'reduceSum(function(d){{return d["{name}"];}})'.format(name=self.json_group_column)
+        elif method == "mean":
+            return REDUCE_MEAN.format(name=self.json_group_column)
+        else:
+            raise ValueError("unknown reduce method")
+
+    @property
+    def reduce_method(self):
+        return self.get_setting("reduce", "count")
 
 
 class BarChart(DCChart):
@@ -87,7 +133,7 @@ class BarChart(DCChart):
 
     @property
     def bins(self):
-        return self.settings.get("bins", 30)
+        return self.get_setting("bins", 30)
 
     @property
     def dimension(self):
@@ -100,13 +146,13 @@ class RowChart(DCChart):
 
     @property
     def dimension(self):
-        dt = self.dt
-        if dt is not None:
-            if dt == "year":
+        transform = self.get_setting("transform", None)
+        if transform is not None:
+            if transform == "year":
                 expr = 'return d3.time.year(d["{name}"]).getFullYear()'
-            elif dt == "month":
+            elif transform == "month":
                 expr = 'return d["{name}"].getMonth() + 1'
-            elif dt == "dayofweek":
+            elif transform == "dayofweek":
                 expr = 'var day = d["{name}"].getDay();' \
                        'var name=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];' \
                         'return day+"."+name[day];'
@@ -116,34 +162,80 @@ class RowChart(DCChart):
             expr = 'return d["{name}"]'
         return expr.format(name=self.json_dim_column)
 
+
+class PieChart(RowChart):
+    type = "pieChart"
+
     @property
-    def dt(self):
-        return self.settings.get("dt", None)
+    def radius(self):
+        return self.get_setting("radius", min(self.width, self.height) * 0.5)
+
+    @property
+    def inner_radius(self):
+        return self.get_setting("inner_radius", self.radius * 0.7)
+
+
+class ChartCommandParser(object):
+
+    def __init__(self, cmd):
+        self.chart_type = "|"
+        self.dim_column = ""
+        self.group_column = ""
+        self.dim_transform = None
+        self.group_reduce = "count"
+
+        if cmd[1] == ":":
+            self.chart_type = cmd[0]
+            cmd = cmd[2:]
+        idx = cmd.find("~")
+        if idx == -1:
+            self.dim_column = cmd
+        else:
+            self.dim_column = cmd[:idx]
+        if ">" in self.dim_column:
+            self.dim_column, self.dim_transform = self.dim_column.split(">")
+        if idx == -1:
+            self.group_column = self.dim_column
+            self.group_reduce = "count"
+        else:
+            cmd = cmd[idx+1:]
+            idx = cmd.index("(")
+            self.group_reduce = cmd[:idx]
+            self.group_column = cmd[idx+1:-1]
+
+
+def create_chart(chart_cmd):
+    chart_map = {
+        "|": BarChart,
+        "=": RowChart,
+        "O": PieChart,
+    }
+
+    parser = ChartCommandParser(chart_cmd)
+    klass = chart_map[parser.chart_type]
+    chart = klass(parser.dim_column, parser.group_column,
+                  transform=parser.dim_transform,
+                  reduce=parser.group_reduce)
+    return chart
 
 
 class CrossFilter(object):
 
-    def __init__(self, df, columns=None, charts=None, **settings):
-        if columns is None:
-            columns = df.columns.tolist()
-        self.df = df[[col.split("@")[0] for col in columns]]
+    def __init__(self, df, charts=None, **settings):
 
-        if charts is None:
-            self.charts = []
+        self.charts = [create_chart(chart) if isinstance(chart, (str, unicode)) else chart for chart in charts]
+        columns = set()
+        for chart in self.charts:
+            columns.add(chart.dim_column)
+            columns.add(chart.group_column)
+        columns = list(columns)
 
-            for column in columns:
-                col = column.split("@")[0]
-                if "@" in column:
-                    if self.df[col].dtype.name.startswith("datetime"):
-                        dt = column.split("@")[1]
-                    else:
-                        dt = None
-                    chart = RowChart(self.df, col, dt=dt, **settings)
-                else:
-                    chart = BarChart(self.df, col, **settings)
-                self.charts.append(chart)
-        else:
-            self.charts = charts
+        self.df = df[columns]
+
+        for chart in self.charts:
+            chart.filter = self
+
+        self.settings = settings
 
         now = datetime.now()
         self.timestamp = now.strftime("%y%m%d%H%M%S%f")
@@ -193,5 +285,6 @@ def nb_crossfilter(in_name, out_name, **kw):
                            in_name=in_name,
                            out_name=out_name)
     display.display_html(html, raw=True)
+    return html
 
 #.x(d3.scale.linear().domain([{{filter.col_min(i)}}, {{filter.col_max(i)}}]))
